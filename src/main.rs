9625786940ae97c7516532f5a87c7b8365824cdc
puripuri2100 <puripuri2100@gmail.com>
2022-02-08 14:22:34 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use actix_web::{App, HttpServer};
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new())
+  HttpServer::new(App::new)
     .bind("127.0.0.1:55555")?
     .run()
     .await
