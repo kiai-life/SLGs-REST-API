78240adbb14632347325c168becaeb56ee7badd8
puripuri2100 <puripuri2100@gmail.com>
2022-02-10 22:53:39 +0900

    天気取得APIを実装

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+use actix_web::{
+  client::{Client, Connector},
+  Error,
+};
+use openssl::ssl::{SslConnector, SslMethod};
+use serde_json;
 
+pub mod weather;
+
+async fn url_get_json(url: &str) -> Result<serde_json::Value, Error> {
+  let builder = SslConnector::builder(SslMethod::tls()).unwrap();
+  let client = Client::builder()
+    .connector(Connector::new().ssl(builder.build()).finish())
+    .finish();
+  let mut res = client.get(url).send().await?;
+  let res_json = res.json::<serde_json::Value>().await?;
+  Ok(res_json)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use actix_web::{App, HttpServer};
+use actix_web::{get, App, Error, HttpResponse, HttpServer};
+
+use slgs_rest_api::weather;
+
+#[get("/ping")]
+async fn index() -> Result<HttpResponse, Error> {
+  Ok(
+    HttpResponse::ExpectationFailed()
+      .content_type("application/json")
+      .body(r##"{"msg": "pong"}"##),
+  )
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(App::new)
+  HttpServer::new(|| App::new().service(index).service(weather::get_weather))
     .bind("127.0.0.1:55555")?
     .run()
     .await
