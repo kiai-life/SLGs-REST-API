e04a1675d1b34806c032ba0b6496b9dc6eee18ce
puripuri2100 <puripuri2100@gmail.com>
2022-02-13 00:20:38 +0900

    環境変数を使うように設定

--- src/db.rs
+++ src/db.rs
@@ -2,10 +2,33 @@ use data_encoding::HEXUPPER;
 use postgres::{Client, Error, NoTls};
 use ring::digest::{digest, SHA256};
 
+const POSTGRESQL_HOST_KEY: &str = "SLGs_POSTGRESQL_HOST";
+const DEFAULT_POSTGRESQL_HOST: &str = "localhost";
+
+const POSTGRESQL_USER_KEY: &str = "SLGs_POSTGRESQL_USER";
+const DEFAULT_POSTGRESQL_USER: &str = "postgres";
+
+const POSTGRESQL_PASSWORD_KEY: &str = "SLGs_POSTGRESQL_PASSWORD";
+const DEFAULT_POSTGRESQL_PASSWORD: &str = "postgres";
+
+const POSTGRESQL_DB_NAME_KEY: &str = "SLGs_POSTGRESQL_DB_NAME";
+const DEFAULT_POSTGRESQL_DB_NAME: &str = "slgs_db";
+
 pub async fn register_user_data(id: &str, email: &str, password: &str) -> Result<(), Error> {
+  let postgresql_host =
+    std::env::var(POSTGRESQL_HOST_KEY).unwrap_or_else(|_| DEFAULT_POSTGRESQL_HOST.to_string());
+  let postgresql_user =
+    std::env::var(POSTGRESQL_USER_KEY).unwrap_or_else(|_| DEFAULT_POSTGRESQL_USER.to_string());
+  let postgresql_password = std::env::var(POSTGRESQL_PASSWORD_KEY)
+    .unwrap_or_else(|_| DEFAULT_POSTGRESQL_PASSWORD.to_string());
+  let postgresql_db_name = std::env::var(POSTGRESQL_DB_NAME_KEY)
+    .unwrap_or_else(|_| DEFAULT_POSTGRESQL_DB_NAME.to_string());
   // https://docs.rs/postgres/latest/postgres/config/struct.Config.html
   // を見よ
-  let mut client = Client::connect("host=localhost user=postgres password=postgres", NoTls)?;
+  let mut client = Client::connect(
+    &format!("host={postgresql_host} user={postgresql_user} password={postgresql_password} dbname={postgresql_db_name}"),
+    NoTls,
+  )?;
   client.batch_execute(
     "
     CREATE TABLE IF NOT EXISTS login_data (
